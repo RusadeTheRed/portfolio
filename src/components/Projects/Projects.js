0aceceffedd804de998fad2ab3a03f6b3499b7ab
Randell Anderson <zugzug07@gmail.com>
2021-09-06 15:25:25 -0500

    Test Projects for setup

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,54 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(({id, image, title, description, tags, source, visit}) => (
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, i) => (
+                <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={visit}>Code</ExternalLinks>
+            <ExternalLinks href={source}>Source</ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
+
+// const projectsDemo = [
+//         {
+//           title: "project 1",
+//           description: "description of project",
+//         },
+//         {
+//           title: "project 2",
+//           description: "description of project",
+//         },
+//         {
+//           title: "project 3",
+//           description: "description of project",
+//         },
+//         {
+//           title: "project 4",
+//           description: "description of project",
+//         },
+//       ]
